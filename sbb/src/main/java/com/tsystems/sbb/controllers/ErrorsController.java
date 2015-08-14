bc7788e478bc4d8b6e55064a61e3f1459ada241e
rdaumbaev <ruslan.daumbaev@gmail.com>
2015-08-14 15:24:05 +0300

    add trains edit functionality

--- sbb/src/main/java/com/tsystems/sbb/controllers/ErrorsController.java
+++ sbb/src/main/java/com/tsystems/sbb/controllers/ErrorsController.java
@@ -1,14 +1,28 @@
 package com.tsystems.sbb.controllers;
 
 import com.tsystems.sbb.exceptions.ResourceNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@ControllerAdvice
 public class ErrorsController {
+
+    private static Logger log = LogManager.getLogger(ErrorsController.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public String handleResourceNotFoundException() {
-        return "services/notfound";
+        log.error("Resource not found");
+        return "service/notfound";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleAllException(Exception ex) {
+        log.error("Exception", ex);
+        return "service/error";
     }
 }
---
+++ sbb/web/WEB-INF/views/service/error.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rdaumbae
+  Date: 14.08.2015
+  Time: 14:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+
+<h1>Aaaaaa!!!! Something bad has happened!!! We're gonna die!</h1>
+</body>
+</html>
